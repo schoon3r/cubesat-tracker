54593b5393c3b8f96d28fc854e4440e83c011b01
Jim Labilles <jim@labilles.com.au>
2024-05-16 11:01:56 +0930

    update for specific time data pull

--- cubesat-tracker.py
+++ cubesat-tracker.py
@@ -9,10 +9,10 @@ from skyfield.api import EarthSatellite, load, wgs84
 
 # Define TLE parameters
 ts = load.timescale()
-line1 = '1 45727U 20037E   24135.43144721  .00005438  00000+0  47906-3 0  9996'
-line2 = '2 45727  97.7757 312.0535 0010345 230.6380 129.3926 14.96842205210896'
+line1 = '1 45727U 20037E   24136.43417487  .00007361  00000+0  64613-3 0  9990' # TLE Line 1
+line2 = '2 45727  97.7758 313.0504 0010341 226.8310 133.2047 14.96857924211040' # TLE Line 2
 satellite = EarthSatellite(line1, line2, 'M2 PATHFINDER', ts)
-print("[+] Tracking...")
+print("[+] Tracking Satellite:")
 print(satellite)
 
 # Define observer's location (Adelaide SA 5000)
@@ -25,9 +25,9 @@ ts = load.timescale()
 t = ts.now()
 
 # Find when cubesat rises and sets
-bluffton = wgs84.latlon(+40.8939, -83.8917)
-t0 = ts.utc(2014, 1, 23)
-t1 = ts.utc(2014, 1, 24)
+bluffton = wgs84.latlon(observer_lat, observer_lon)
+t0 = ts.utc(2024, 5, 14)
+t1 = ts.utc(2024, 5, 15)
 t, events = satellite.find_events(bluffton, t0, t1, altitude_degrees=30.0)
 event_names = 'rise above 30�', 'culminate', 'set below 30�'
 print("\n[+] Cubesat Rises and Sets")
@@ -36,7 +36,7 @@ for ti, event in zip(t, events):
     print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
 
 # Generating Cubesat Position
-t = ts.now()
+t = ts.now() # Define position according to certain time
 geocentric = satellite.at(t)
 print("\n[+] Cubesat Position")
 print(geocentric.position.km)
